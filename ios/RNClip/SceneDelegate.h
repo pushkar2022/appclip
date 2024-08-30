bdebcdba1283756bf8de29efe7205073c7503ece
harshit <harshit@dev.familyone.io>
2024-08-30 16:27:42 +0530

    app Clip

---
+++ ios/RNClip/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  RNClip
+//
+//  Created by pushkar kumar on 30/08/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ ios/RNClip/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  RNClip
+//
+//  Created by pushkar kumar on 30/08/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
